ec18fb43e908d630111d492a8a29ac726b56ea1d
Ilya Cherepanov <cherepanov.ilya@gmail.com>
2024-06-06 19:21:41 +0700

    feat(user-edit): add form validation

--- src/app/componens/user-edit/user-edit.component.ts
+++ src/app/componens/user-edit/user-edit.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { User } from '../../types';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-user-edit',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './user-edit.component.html',
   styleUrl: './user-edit.component.scss'
 })
@@ -15,10 +16,20 @@ export class UserEditComponent implements OnInit {
   @Output() onCancel = new EventEmitter();
 
   userForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    email: new FormControl(''),
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
   });
+
+  get firstName() {
+    return this.userForm.get('firstName');
+  }
+  get lastName() {
+    return this.userForm.get('lastName');
+  }
+  get email() {
+    return this.userForm.get('email');
+  }
 
   ngOnInit(): void {
     this.userForm.setValue({
@@ -34,11 +45,13 @@ export class UserEditComponent implements OnInit {
   }
 
   submitForm() {
-    this.onSubmit.emit({
-      ...this.user,
-      firstName: this.userForm.value.firstName ?? '',
-      lastName: this.userForm.value.lastName?? '',
-      email: this.userForm.value.email ?? '',
-    });
+    if (this.userForm.valid) {
+      this.onSubmit.emit({
+        ...this.user,
+        firstName: this.userForm.value.firstName ?? '',
+        lastName: this.userForm.value.lastName ?? '',
+        email: this.userForm.value.email ?? '',
+      });
+    }
   }
 }
